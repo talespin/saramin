7f351696b082ed90dbf0b42bac92ad27b8ccad61
Choi Jonghwan <newtale@naver.com>
2024-01-24 15:27:13 +0900

    Create 2.saramin_split.py

---
+++ src/2.saramin_split.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+"""
+:filename: 2.jobkorea_split.py
+:author: 최종환
+:last update: 2024.01.20
+ 
+:CHANGELOG:
+    ============== ========== ====================================
+    수정일            수정자        수정내용
+    ============== ========== ====================================
+    2024.01.20     최종환        최초생성
+    ============== ========== ====================================
+ 
+:desc:
+    saramin site 의 체용정보 목록을 입력하는 숫자에 맞춰서 나눈다
+ 
+"""
+import os
+import math
+import pandas as pd
+
+
+def main():
+    split_cnt = int(input("\r\n\r\n몇개의 파일로 나눌까요?\r\n(숫자만입력해주세요)"))
+    if not os.path.exists('../list/saramin.xlsx'):
+        print(os.path.abspath('../list/saramin.xlsx') + ' 파일이 없습니다. saramin_list.py 를 실행해서 먼저 리스트파일을 생성하세요')
+        return
+    df = pd.read_excel('../list/jobkorea.xlsx')
+    if len(df) < split_cnt:
+        print('건수보다 나누려는 수가 큽니다.')
+        return
+    page_cnt = math.ceil(len(df) / split_cnt)
+    for cnt in range(0, split_cnt):
+        df[cnt *page_cnt:(cnt+1)*page_cnt].to_json(f'../list/saramin_{cnt+1}.json', orient='records', force_ascii=False)
+
+
+
+if __name__=='__main__':
+    main()
+
