898a07433b86eb2ccb7787919dd7c24c1d956431
Choi Jonghwan <newtale@naver.com>
2024-01-25 16:23:48 +0900

    Update 3.saramin_crawler.py

--- src/3.saramin_crawler.py
+++ src/3.saramin_crawler.py
@@ -29,61 +29,65 @@ from bs4 import BeautifulSoup as bs
 #from selenium.webdriver.chrome.service import Service
 #from selenium.webdriver.chrome.options import Options
 
-
 def saram_crawler(list_file:str, overwrite:bool = False):
     os.makedirs('../crawl', exist_ok=True) 
     if not os.path.exists(list_file):
         print('File not found:' + os.path.abspath(list_file))
         return
-    headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Cache-Control': 'max-age=0',
-        "Connection": 'keep-alive',
-        "Host": 'www.saramin.co.kr',
-        "Referer": 'https://www.saramin.co.kr/zf_user/jobs/list/domestic',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        "sec-ch-ua-mobile": '?0',
-        'sec-ch-ua-platform': "macOS",
-    }
-    params = {
-        'view_type': 'list',
-        'rec_idx': 'id',
-        'location': 'ts',
-        'searchType': 'search',
-        'paid_fl': 'n',
-        'inner_source': 'saramin',
-        'inner_medium': 'pattern',
-        'inner_campaign': 'SRI_050_ARA_MTRX_RCT',
-        'inner_term': '1',
-    }
-    #세션 및 쿠키생성
-    session = req.Session()
+    items = pd.read_json(list_file).to_dict('records')
     base_url = 'https://www.saramin.co.kr'
+    headers = {
+        'Accept': 'text/html, */*; q=0.01',
+        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin': 'https://www.saramin.co.kr',
+        'Referer': 'https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx=47370025&recommend_ids=eJxdjsENQzEIQ6fpHYOB%2BNxBsv8Wza%2FUJKo4PRts2AG36jmAV7%2FZNEfFlPkfTn4FZ6ix16PCBxf2D0k%2FOExVZ1mpzp0dOTS0XZeCx7VK1xW1WkMb6blmI0Dj9QZrKRs9O7vOLYCR57YSfZLd0ci7KMT7DaU9%2BAHvZUQ%2F&view_type=list&gz=1&t_ref_content=general&t_ref=area_recruit&relayNonce=6086dd278acbb29270a8&immediately_apply_layer_open=n',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+    }
+    session = req.Session()
     res = session.get(base_url, headers=headers, verify=False)
     cookies = dict(res.cookies)
-    items = pd.read_json(list_file).to_dict('records')
+    data = {
+        'rec_idx': '47107294',
+        'rec_seq': '2',
+        'utm_source': '',
+        'utm_medium': '',
+        'utm_term': '',
+        'utm_campaign': '',
+        'view_type': 'list',
+        't_ref': 'area_recruit',
+        't_ref_content': 'general',
+        't_ref_scnid': '902',
+        'search_uuid': '',
+        'refer': '',
+        'searchType': '',
+        'searchword': '',
+        'ref_dp': 'SRI_050_VIEW_MTRX_RCT_NOINFO',
+        'dpId': '',
+        'recommendRecIdx': '',
+    }
     for item in items:
-        id = item['id']
-        file_name = f'../crawl/{id}/{id}.html'
+        id = item['id'].split('-')[-1]
+        file_name = f'../crawl/{id}.html'
+        data.update({'rec_idx':id})
         if os.path.exists(file_name):
-            logging.info(f'    Skip exists file : {file_name}')
+            print(f'    Skip file : {file_name}')
             continue
-        while True:
-            logging.info(f'    Crawling {file_name} ....')
-            params.update({'rec_idx':id})
-            res = session.get(f'{base_url}/zf_user/jobs/relay/view', params=params, headers=headers, verify=False, cookies=cookies)
-            sleep(5)
-            if res.status_code == 200: break
-            sleep(10)
-
+        res = req.post(f'{base_url}/zf_user/jobs/relay/view-ajax', cookies=cookies, headers=headers, data=data)
+        if res.status_code != 200:
+            print(' error 발생')
+            raise
+        with open(file_name, 'wt', encoding='utf-8') as fs:
+            fs.write(res.content.decode('utf-8'))
+        sleep(5)
 
 
 if __name__=='__main__':
