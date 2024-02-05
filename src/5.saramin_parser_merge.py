a6e31effddff7a2cf582b45e8b5d2be7bb3067c3
talespin <newtale@naver.com>
2024-02-05 13:03:38 +0900

    parsed file merge

---
+++ src/5.saramin_parser_merge.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import orjson as json
+from glob import glob
+
+
+result = []
+for file_name in glob('../result/*.json'):
+    try:
+        with open(file_name, 'rt', encoding='utf-8') as fs:
+            result.append(json.loads(fs.read()))
+    except:
+        print(f'parse error:{file_name}')
+
+df = pd.DataFrame(result)
+df.to_csv('../result/saramin.csv')
+df.to_excel('../result/saramin.xlsx')
+
+
