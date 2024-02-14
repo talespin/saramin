6ddfb54fa52e8e71628d4f4b6539327b7178485d
Choi Jonghwan <newtale@naver.com>
2024-02-14 13:23:12 +0900

    Update crawl_check.py

--- src/crawl_check.py
+++ src/crawl_check.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from time import sleep
 from os.path import exists
+from datetime import datetime
 
 df = pd.read_excel('../list/saramin.xlsx')
 df['exists'] = False
 while True:
     df['exists'] = df.apply(lambda x:x['exists'] if x['exists'] else exists(f'../crawl/{x["id"]}/{x["id"]}.html'), axis=1)
-    print(f'남은크롤갯수 : {len(df[df["exists"]==False])}  / {len(df)}')
+    print(f'{datetime.now()}  남은크롤갯수 : {len(df[df["exists"]==False])}  / {len(df)}')
     sleep(60)
 
