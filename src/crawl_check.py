3cc17ac789a558e49803cc1f7c4931df371e243a
Choi Jonghwan <newtale@naver.com>
2024-02-14 13:09:58 +0900

    Create crawl_check.py

---
+++ src/crawl_check.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from time import sleep
+from os.path import exists
+
+df = pd.read_excel('../list/saramin.xlsx')
+df['exists'] = False
+while True:
+    df['exists'] = df.apply(lambda x:x['exists'] if x['exists'] else exists(f'../crawl/{x["id"]}/{x["id"]}.html'), axis=1)
+    print(len(df[df['exists']==False]) + ' / ' + len(df))
+    sleep(60)
+
