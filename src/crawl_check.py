84a2699a7a19c7205052106a59e64e6304f581c4
Choi Jonghwan <newtale@naver.com>
2024-02-14 13:14:28 +0900

    Update crawl_check.py

--- src/crawl_check.py
+++ src/crawl_check.py
@@ -6,6 +6,6 @@ df = pd.read_excel('../list/saramin.xlsx')
 df['exists'] = False
 while True:
     df['exists'] = df.apply(lambda x:x['exists'] if x['exists'] else exists(f'../crawl/{x["id"]}/{x["id"]}.html'), axis=1)
-    print(f'{len(df[df['exists']==False])}  / {len(df)})
+    print(f'{len(df[df["exists"]==False])}  / {len(df)})
     sleep(60)
 
