daa94a0612e833219d96c6c8f1938365a0e86d1d
talespin <newtale@naver.com>
2024-02-16 09:40:14 +0900

    crawler shell modify

--- src/saramin_crawler.sh
+++ src/saramin_crawler.sh
@@ -4,7 +4,9 @@ cd /mnt/work/saramin/src
 
 while :
 do
-	python 1.saramin_list.py -y=yes;python 3.saramin_crawler_master.py
-        sleep 1h
+    python 1.saramin_list.py -y=yes
+    python 3.saramin_crawler_master.py
+    echo "1시간 쉬었다가 다시 시작합니다."
+    sleep 1h
 done
 
