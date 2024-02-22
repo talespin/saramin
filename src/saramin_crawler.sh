c4a3d48c4fa93aebdb191bc53dfa87d5f563cce6
talespin <newtale@naver.com>
2024-02-22 09:04:17 +0900

    shell 변경

--- src/saramin_crawler.sh
+++ src/saramin_crawler.sh
@@ -4,8 +4,8 @@ cd /mnt/work/saramin/src
 
 while :
 do
-    python 1.saramin_list.py -y=yes
-    python 3.saramin_crawler_master.py
+    python -u 1.saramin_list.py -y=yes
+    python -u 3.saramin_crawler_master.py
     echo `date`
     echo "1시간 쉬었다가 다시 시작합니다."
     sleep 1h
